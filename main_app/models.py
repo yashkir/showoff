80ecff7d3efdf9be65d8577ead2b855408ca74e9
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-05-17 11:23:02 -0400

    set up the models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Collection(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    is_public = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+
+class Item(models.Model):
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    description = models.TextField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class Picture(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    text = models.TextField(1024)
+
+    def __str__(self):
+        return self.text[:64]
