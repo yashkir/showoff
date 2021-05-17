7941d3e39f331661b7d5ea1b7ef06e8b523d16f9
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-05-17 12:14:16 -0400

    add collections/ index

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 
+from .models import Collection
+
+
 def home(request):
     return render(request, 'home.html')
+
+def collections_index(request):
+    collections = Collection.objects.all()
+    return render(request, 'collections/index.html', { 'collections': collections })
