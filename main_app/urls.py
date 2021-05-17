d44c6ea0b37f5b89654394e5c155ba2e94f5f6ba
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-05-17 14:40:31 -0400

    add My Collections to filter by user

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('collections/', views.collections_index, name='collections_index'),
+    path('collections/user/<int:user_id>/', views.collections_index_by_user, name='collections_index_by_user'),
     path('collections/<int:collection_id>/', views.collections_detail, name='collections_detail'),
     path('items/<int:item_id>/', views.items_detail, name='items_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -10,6 +10,10 @@ def collections_index(request):
     collections = Collection.objects.all()
     return render(request, 'collections/index.html', { 'collections': collections })
 
+def collections_index_by_user(request, user_id):
+    collections = Collection.objects.filter(user=user_id)
+    return render(request, 'collections/index.html', { 'collections': collections })
+
 def collections_detail(request, collection_id):
     collection = Collection.objects.get(id=collection_id)
     items = Item.objects.filter(collection=collection_id)
