3cee123f9c2c319968e3358faa7da1b10c9bdca0
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-05-19 12:56:25 -0400

    style comment form with bootstrap 'form-control'

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,8 +1,13 @@
+from django.forms.widgets import TextInput
 from django.forms import ModelForm
 from .models import Comment
 
 
 class CommentForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ModelForm, self).__init__(*args, **kwargs)
+        self.fields['text'].widget.attrs = {'class': 'form-control'}
+
     class Meta:
         model = Comment
         fields = ['text']
